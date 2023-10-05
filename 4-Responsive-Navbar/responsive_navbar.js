28f3436ced824aa490f26b3eea8c74cb80e96541
Bahadır Hakan <bhyuksel0@gmail.com>
2023-10-05 23:17:17 +0200

    4-Responsive-Navbar

--- 4-Responsive-Navbar/responsive_navbar.js
+++ 4-Responsive-Navbar/responsive_navbar.js
@@ -1,14 +1,25 @@
 const responsive_btn = document.getElementById('responsive_btn')
 const menu = document.getElementById('menu')
+const html = document.querySelector('html')
+const nav_titles = document.querySelectorAll('.nav_title')
 
 responsive_btn.addEventListener('click',() => {
     if(menu.style.height != "calc(100vh - 5rem)"){
         menu.style.height = "calc(100vh - 5rem)"
         menu.style.visibility = 'visible'
         menu.style.opacity = '1'
-    }else{
-        menu.style.height = ""
-        menu.style.visibility = ''
-        menu.style.opacity = ''
-    }
+    }else closeTheResponsiveMenu()
 })
+
+nav_titles.forEach(title => {
+    title.addEventListener('click',() => {
+        if(menu.style.height === "calc(100vh - 5rem)") closeTheResponsiveMenu()
+    })
+})
+
+function closeTheResponsiveMenu(){
+    menu.style.height = ""
+    menu.style.visibility = ''
+    menu.style.opacity = ''
+    html.style.overflow = 'scroll'
+}
